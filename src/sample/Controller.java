343854ca9cf5cc032c29af222a2dbc214e385da6
Michael Hamami <hamami2010@gmail.com>
2019-05-05 02:47:47 +0300

    Commit Message: need to manage Controller for the UI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class Controller {
+
+    @FXML
+    public TableColumn nameColumn;
+    public TableColumn dateColumn;
+    public TableColumn timeColumn;
+    public TableView tableTask;
+
+
 }
---
+++ src/sample/Task.java
@@ -0,0 +1,51 @@
+package sample;
+
+import java.util.Date;
+
+public class Task {
+
+    private Date date;
+    private String name;
+    private String time;
+    private int numberTask;
+
+
+    public Task(Date date, String name, String time, int numberTask) {
+        this.date = date;
+        this.name = name;
+        this.time = time;
+        this.numberTask = numberTask;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getNumberTask() {
+        return numberTask;
+    }
+
+    public void setNumberTask(int numberTask) {
+        this.numberTask = numberTask;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
